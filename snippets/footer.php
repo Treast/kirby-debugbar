9a5e219efbcabf1e681b4f8935508a4b0bdabeae
Vincent Riva <vincent.riva@outlook.fr>
2023-05-01 19:45:41 +0200

    💄 Customize PHP Debug Bar logo

--- snippets/footer.php
+++ snippets/footer.php
@@ -5,4 +5,5 @@ use Treast\KirbyDebugbar\Debugbar;
 $renderer = Debugbar::getRenderer('/media/plugins/treast/debugbar');
 
 echo Debugbar::getRenderer()->renderHead();
+echo css('/media/plugins/treast/debugbar/index.css');
 echo Debugbar::getRenderer()->render();
