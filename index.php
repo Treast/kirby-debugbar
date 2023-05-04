bcae9b62534769d59edba72457ad3537af977910
Bruno Meilick <bnomei@users.noreply.github.com>
2023-05-04 09:12:08 +0100

    more option debug changes

--- index.php
+++ index.php
@@ -7,17 +7,23 @@ use Kirby\Cms\App as Kirby;
 Kirby::plugin('treast/debugbar', [
     'hooks' => [
         'system.loadPlugins:after' => function () {
-            \Treast\KirbyDebugbar\Debugbar::init(kirby());
+            if (option('debug')) {
+                \Treast\KirbyDebugbar\Debugbar::init(kirby());
+            }
         },
         'page.render:before' => function (string $contentType, array $data, \Kirby\Cms\Page $page) {
-            \Treast\KirbyDebugbar\Debugbar::logPage($page);
+            if (option('debug')) {
+                \Treast\KirbyDebugbar\Debugbar::logPage($page);
+            }
             return $data;
         },
         '*:after' => function (\Kirby\Cms\Event $event) {
-            \Treast\KirbyDebugbar\Debugbar::logEvent($event);
+            if (option('debug')) {
+                \Treast\KirbyDebugbar\Debugbar::logEvent($event);
+            }
         },
         'system.exception' => function (Throwable $exception) {
-            if ($exception instanceof Exception) \Treast\KirbyDebugbar\Debugbar::logException($exception);
+            if (option('debug') && $exception instanceof Exception) \Treast\KirbyDebugbar\Debugbar::logException($exception);
         },
     ],
     'snippets' => [
