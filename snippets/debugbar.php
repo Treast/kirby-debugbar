444ffdd414d81e702b6c95511c146778590f2bc9
Bruno Meilick <bnomei@users.noreply.github.com>
2023-05-04 09:03:19 +0100

    debugbar only when kirby option debug is true

--- snippets/debugbar.php
+++ snippets/debugbar.php
@@ -1,9 +1,11 @@
 <?php
 
-use Treast\KirbyDebugbar\Debugbar;
+if (option('debug')) {
+  use Treast\KirbyDebugbar\Debugbar;
 
-$renderer = Debugbar::getRenderer('/media/plugins/treast/debugbar');
+  $renderer = Debugbar::getRenderer('/media/plugins/treast/debugbar');
 
-echo Debugbar::getRenderer()->renderHead();
-echo css('/media/plugins/treast/debugbar/index.css');
-echo Debugbar::getRenderer()->render();
+  echo Debugbar::getRenderer()->renderHead();
+  echo css('/media/plugins/treast/debugbar/index.css');
+  echo Debugbar::getRenderer()->render();
+}
